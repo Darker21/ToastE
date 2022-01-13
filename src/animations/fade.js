b454411e0ef4168eca0f0da0e542e32d60fdd11e
Jacob Darker <41199879+Darker21@users.noreply.github.com>
2022-01-13 22:55:11 +0000

    Part way through transpiling

---
+++ src/animations/fade.js
@@ -0,0 +1,52 @@
+/**
+ * Fade an HTML Element into view
+ * @param {HTMLElement} element - The element to fade in
+ * @param {Number} duration - The duration of the fade animation
+ * @param {Function} callback - The callback function after the animation has completed
+ * @param {any[]} params - The parameters for the callback function
+ * @see {@link https://codepen.io/jorgemaiden/pen/xoRKWN}
+ */
+function fadeIn(element, duration, callback = null) {
+    element.style.display = '';
+    element.style.opacity = 0;
+    var last = +new Date();
+    var tick = function () {
+        element.style.opacity = +element.style.opacity + (new Date() - last) / duration;
+        last = +new Date();
+        if (+element.style.opacity < 1) {
+            window.requestAnimationFrame && requestAnimationFrame(tick) || setTimeout(tick, 16);
+        } else {
+            if (callback) {
+                callback.call();
+            }
+        }
+    };
+    tick();
+}
+
+/**
+ * Fade an HTML Element out of view
+ * @param {HTMLElement}} element - The element to fade out
+ * @param {Number} duration - The duration of the fade animation
+ * @param {Function} callback - The callback function called after the animation has completed
+ */
+function fadeOut(element, duration, callback = null) {
+    element.style.display = '';
+    element.style.opacity = 1;
+    var last = +new Date();
+    var tick = function () {
+        element.style.opacity = +element.style.opacity - (new Date() - last) / duration;
+        last = +new Date();
+        if (+element.style.opacity > 0) {
+            window.requestAnimationFrame && requestAnimationFrame(tick) || setTimeout(tick, 16);
+        } else {
+            if (callback) {
+                callback.call();
+            }
+        }
+    };
+    tick();
+}
+
+
+export { fadeIn, fadeOut };
---
+++ src/animations/slide.js
@@ -0,0 +1,20 @@
+// Convert this css to JS https://jsfiddle.net/cferdinandi/qgpxvhhb/23/
+
+function slideUp(element, duration, callback = null) {
+
+}
+
+function slideDown(element, duration, callback = null) {
+
+}
+
+function slideLeft(element, duration, callback = null) {
+
+}
+
+function slideRight(elemnt, duration, callback = null) {
+
+}
+
+
+export { slideDown, slideLeft, slideRight, slideUp };
