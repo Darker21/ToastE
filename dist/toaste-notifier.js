ec2d86efda3d93a3b0a1abce7131c48eb670b6d2
Jacob Darker <41199879+Darker21@users.noreply.github.com>
2022-01-22 20:52:09 +0000

    Update linting and code analysis settings

--- dist/toaste-notifier.js
+++ dist/toaste-notifier.js
@@ -6,8 +6,8 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["ToastE-Notifier"] = factory());
-})(this, (function () { 'use strict';
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global['ToastE-Notifier'] = factory());
+}(this, (function () { 'use strict';
 
   function _typeof(obj) {
     "@babel/helpers - typeof";
@@ -818,4 +818,4 @@
 
   return ToastENotifier;
 
-}));
+})));
